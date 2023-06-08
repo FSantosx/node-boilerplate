7e81b460a045440eba809a08a42c8e4c6d350e31
Fabio Santos <fabiojunior96@outlook.com>
2023-06-07 21:29:03 -0300

    created a ejs structure

---
+++ src/app.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+const path = require('path')
+const express = require('express')
+const app = express()
+const routes = require('./routes/routes')
+const port = process.env.PORT || 5000
+
+app.use(routes)
+app.use(express.static(__dirname + '/webroot/'));
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'webroot/views/'));
+app.listen(port, () => console.log(`app listening on port ${port}!`))
